f99c58739f1d2f520b366f0cb5c0203c0cafecbf
Kellen Lask <kellenlask@gmail.com>
2018-07-02 22:44:20 -0600

    Repl Project - I haven't used this yet, but I know I'll want it

---
+++ Repl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Repl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
