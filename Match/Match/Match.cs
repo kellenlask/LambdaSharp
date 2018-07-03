759717eac6ae77ae141b3b3e86f25e4f30a0eb35
Kellen Lask <kellenlask@gmail.com>
2018-07-02 22:45:52 -0600

    Pattern Matching I feel like there's still room to improve this, but where it's at is very acceptable.

---
+++ Match/Match/Match.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Match
+{
+    public class Match<TIn, TOut>
+    {
+        private Func<TIn, TOut> _default = val => default(TOut);
+
+        private readonly List<(Predicate<TIn>, Func<TIn, TOut>)> _cases = new List<(Predicate<TIn>, Func<TIn, TOut>)>();
+
+
+        public Match<TIn, TOut> When(Predicate<TIn> predicate, Func<TIn, TOut> fn)
+        {
+            _cases.Add((predicate, fn));
+            return this;
+        }
+
+
+        public Match<TIn, TOut> When(Predicate<TIn> predicate, TOut value) => When(predicate, val => value);
+
+
+        public Match<TIn, TOut> When(bool condition, Func<TIn, TOut> value) => When(val => condition, value);
+
+
+        public Match<TIn, TOut> When(bool condition, TOut value) => When(val => condition, val => value);
+
+
+        public Match<TIn, TOut> Otherwise(Func<TIn, TOut> fn)
+        {
+            _default = fn;
+            return this;
+        }
+
+
+        public Match<TIn, TOut> Otherwise(TOut defaultValue) => Otherwise(value => defaultValue);
+
+
+        public TOut For(TIn value)
+        {
+            var foo = _cases.FirstOrDefault(nextCase => nextCase.Item1(value));
+            return (foo.Item2 ?? _default)(value);
+        }
+    }
+}
---
+++ Match/MatchTests/ExampleBased.cs
@@ -0,0 +1,29 @@
+﻿using Match;
+using Xunit;
+
+namespace MatchTests
+{
+    public class ExampleBased
+    {
+        [Fact]
+        public void ItShouldMatch()
+        {
+            var matcher = new Match<string, string>()
+                .When(s => s.Length == 0, "zero")
+                .When(s => s.Length == 1, "one")
+                .When(s => s.Length == 2, "two")
+                .When(s => s.Length == 3, "three")
+                .When(s => s.Length == 4, "four")
+                .When(s => s.Length == 5, "five")
+                .Otherwise(s => s.Length.ToString());
+
+            Assert.Equal(matcher.For(""), "zero");
+            Assert.Equal(matcher.For("1"), "one");
+            Assert.Equal(matcher.For("22"), "two");
+            Assert.Equal(matcher.For("333"), "three");
+            Assert.Equal(matcher.For("4444"), "four");
+            Assert.Equal(matcher.For("55555"), "five");
+            Assert.Equal(matcher.For("666666"), "6");
+        }
+    }
+}
