3f358a26cc9b70f32b0eb635f37de3dcf02ac570
Kellen Lask <kellenlask@gmail.com>
2018-07-02 22:43:52 -0600

    In - Convenience Method for Membership Checking

---
+++ In/In/InExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace In
+{
+    public static class InExtensions
+    {
+        /// <summary>
+        ///     Returns whether or not the given value is in the passed collection of values.
+        ///     <code>
+        ///         if(user.Status.In(Status.Inactive, Status.Disabled, Status.Blacklisted)) {
+        ///             ...
+        ///         }
+        ///     </code>
+        /// </summary>
+        /// <param name="value">The item whose membership you want to test</param>
+        /// <param name="options">The set of items from which membership is determined</param>
+        /// <returns>True/False for membership in the passed set</returns>
+        public static bool In<T>(this T value, params T[] options) => options.Contains(value);
+    }
+}
---
+++ In/InTests/ExampleBased.cs
@@ -0,0 +1,23 @@
+﻿using In;
+using Xunit;
+
+namespace InTests
+{
+    public class ExampleBased
+    {
+        [Fact]
+        public void ItShouldDetectMembership()
+        {
+            var result = "foo".In("foo", "bar", "baz");
+            Assert.True(result);
+        }
+
+
+        [Fact]
+        public void ItShouldReturnFalseForEmptyList()
+        {
+            var result = "foo".In();
+            Assert.False(result);
+        }
+    }
+}
